37224686a4c5ee149789c8444ff1b240edc54a6e
KT1024-kei <kt0120katu@gmail.com>
2022-09-21 00:18:29 +0900

    add Low Path Filter class

--- tools/Mathfunction.py
+++ tools/Mathfunction.py
@@ -76,6 +76,37 @@ class Mathfunction():
   def deriv(self, now, pre,  dt):
     return (now-pre)/dt
 
+## https://www.earlevel.com/main/2003/03/02/the-bilinear-z-transform/
+## https://en.wikipedia.org/wiki/Digital_biquad_filter See transposed direct form
+class LowPath_Filter():
+  
+  def Init_LowPass2D(self, fc): # cut off frecency, sampling rate
+    
+    self.fc = fc
+    self.r0 = np.zeros(3)
+    self.r1 = np.zeros(3)
+    self.r2 = np.zeros(3)
+
+  def LowPass2D(self, V0, dt):
+    fr = 1.0/(self.fc * dt)     
+    omega = np.tan(np.pi/fr)
+
+    c = 1.0 + 2.0/np.sqrt(2)*omega + omega**2
+    b0 = omega**2/c
+    b1 = 2.0*self.b0
+    b2 = self.b0
+
+    a1 = 2*(omega**2 - 1)/c
+    a2 = (1.0 - 2.0/np.sqrt(2)*omega + omega**2)/c
+    
+    self.r0 = V0 - a1*self.r1 - a2*self.r2
+    Vout = b0*self.r0 + b1*self.r1 + b2*self.r2
+
+    self.r2 = self.r1
+    self.r1 = self.r0
+
+    return Vout
+
 class Integration():
   
   def __init__(self, dt, param):
