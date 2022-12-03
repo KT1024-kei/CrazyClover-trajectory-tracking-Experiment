74fa59c1286d6e7b62c9dfcfdd0fab5fb522be4f
KT1024-kei <kt0120katu@gmail.com>
2022-12-03 13:31:20 +0900

    change takeoff and land flow

--- hovering.py
+++ hovering.py
@@ -25,26 +25,31 @@ def Experiment(Texp, Tsam, num_drone):
     zero = np.zeros(3)
     for i in range(num_drone):
         Drone_env[i] = Env_Experiment(Texp, Tsam, i)
-        Drone_ctrl[i] = Controllers(Tsam, "pid", "position")
+        Drone_ctrl[i] = Controllers(Tsam, "mellinger")
 
     for i in range(num_drone):          # ホバリング
-        P = np.array([0.0, 0.0, 0.4])
-        Drone_env[i].hovering(Drone_ctrl[i], P)
+        Drone_env[i].takeoff(Drone_ctrl[i])
         timeHelper.sleep(2)             # 5秒静止
 
     Ts = timeHelper.time()
     Te = -Tsam
     t = 0
 
+    land_flag = True
+    land_time = 10
+
     while True:
 
         for i in range(num_drone):
-
+            Env.set_clock(t)
+            Drone_env[i].set_clock(t)
             Drone_env[i].take_log(t, Drone_ctrl[i])    #　状態と入力を記録
             
-        if t > Texp-3:                      # 着陸
+        if t > Texp:                      # 着陸
             for i in range(num_drone):
-                Drone_env[i].land(Drone_ctrl[i])
+                if land_flag:
+                    Drone_env[i].land_track(Drone_ctrl[i])
+                    land_flag = False
 
         for i in range(num_drone):      # コントローラに状態を渡して入力加速度と角速度を計算
             Drone_ctrl[i].set_state(Drone_env[i].P, Drone_env[i].Vfiltered, Drone_env[i].R, Drone_env[i].Euler)
@@ -52,9 +57,7 @@ def Experiment(Texp, Tsam, num_drone):
         
         for i in range(num_drone):      # ドローンに入力を送信
             input_acc = Drone_ctrl[i].input_acc
-            # input_acc = 8.8
             input_W = np.array([Drone_ctrl[i].input_Wb[0]*1, Drone_ctrl[i].input_Wb[1]*1, Drone_ctrl[i].input_Wb[2]])
-
             cf[i].cmdFullState(zero, 
                             zero, 
                             np.array([0.0, 0.0, input_acc/100.0]), 
@@ -65,7 +68,7 @@ def Experiment(Texp, Tsam, num_drone):
         t = timeHelper.time() - Ts      # ループ周期を一定に維持
         Tsam = t - Te
         Te = t
-        if Env.time_check(t, t - Te, Texp): break
+        if Env.time_check(t - Te, Texp+land_time): break
 
 
         for i in range(num_drone):
